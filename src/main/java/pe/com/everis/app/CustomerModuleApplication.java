10e020f64ac8fdff356c06e2f7b209245c7dc79a
alessandra238 <arosadov@everis.com>
2020-07-23 00:10:26 -0500

    Commit con models

---
+++ src/main/java/pe/com/everis/app/CustomerModuleApplication.java
@@ -0,0 +1,13 @@
+package pe.com.everis.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerModuleApplication.class, args);
+	}
+
+}
